65f7dc8d24cc952689bd850a7af8fe53c51665b7
cl0ud <123465932+cl0udstar@users.noreply.github.com>
2024-07-29 10:43:34 +0100

    added the audio buttons on the selection page

--- scripts/puzzle-selection.js
+++ scripts/puzzle-selection.js
@@ -11,7 +11,8 @@ const arrayData = [
         para: "You begin your mission by looking at some of the data in the zoo. Is it realistic or has it already been tampered with by the hackers.",
         link: "../puzzles/puzzle-1.html",
         lock: "lock1",
-        frontFeatures: "front1"
+        frontFeatures: "front1",
+        audioSrc: "../assets/AudioSample.mp3"
     },
     {
         id: "card-2",
@@ -24,7 +25,8 @@ const arrayData = [
         para: "Help! You now visit the zoo and it is in complete chaos. There are enclosures wide open and there are animals everywhere. There are also no lights working, water is running low and communications are out.",
         link: "../puzzles/puzzle-2.html",
         lock: "lock2",
-        frontFeatures: "front2"
+        frontFeatures: "front2",
+        audioSrc: "../assets/AudioSample.mp3"
     },
     {
         id: "card-3",
@@ -37,7 +39,8 @@ const arrayData = [
         para: "Now that the animals are safely locked back in their enclosures, we need to fix all the systems that the hackers have destroyed.",
         link: "../puzzles/puzzle-3.html",
         lock: "lock3",
-        frontFeatures: "front3"
+        frontFeatures: "front3",
+        audioSrc: "../assets/AudioSample.mp3"
     },
     {
         id: "card-4",
@@ -50,7 +53,8 @@ const arrayData = [
         para: "The Zoo’s sensors have been damaged. We need your help to quickly choose which sensors would be best to replace them.",
         link: "../puzzles/puzzle-4.html",
         lock: "lock4",
-        frontFeatures: "front4"
+        frontFeatures: "front4",
+        audioSrc: "../assets/AudioSample.mp3"
     },
     {
         id: "card-5",
@@ -63,7 +67,8 @@ const arrayData = [
         para: "Quick, the hacker has ramped up the temperature of the reptile house to a hazardous level, the animals are in grave danger!",
         link: "../puzzles/puzzle-5.html",
         lock: "lock5",
-        frontFeatures: "front5"
+        frontFeatures: "front5",
+        audioSrc: "../assets/AudioSample.mp3"
     },
     {
         id: "card-6",
@@ -76,7 +81,8 @@ const arrayData = [
         para: "We are almost there. We have made it to the zoo headquarters.",
         link: "../puzzles/puzzle-6.html",
         lock: "lock6",
-        frontFeatures: "front6"
+        frontFeatures: "front6",
+        audioSrc: "../assets/AudioSample.mp3"
     },
     {
         id: "card-7",
@@ -89,7 +95,8 @@ const arrayData = [
         para: "We need to get the Zoo’s IOT system back up and running and protected. You can help us.",
         link: "../puzzles/puzzle-7.html",
         lock: "lock7",
-        frontFeatures: "front7"
+        frontFeatures: "front7",
+        audioSrc: "../assets/AudioSample.mp3"
     }
 ]
 
@@ -113,11 +120,39 @@ const functionCards = () => {
                                             <h1>MISSION BRIEF</h1>\
                                             <p>'+data.para+'</p>\
                                             <div class="background_back"></div>\
+                                            <div class="audio-controls">\
+                                                <a class="audio-btn button" id="play-pause-btn'+data.cardNum+'" onclick="togglePlayPause('+data.cardNum+')"><i class="fa-solid fa-volume-mute"></i></a>\
+                                                <a class="audio-btn button" id="reset-btn'+data.cardNum+'" onclick="resetAudio('+data.cardNum+')"><i class="fa-solid fa-arrow-rotate-left"></i></a>\
+                                            </div>\
                                             <button class="return" onclick="handle2ndClick'+data.cardNum+'()"><i class="fa-solid fa-arrow-rotate-left"></i></button>\
                                             <a href="'+data.link+'"><button class="start">Start</button></a>\
+                                            <audio id="audio'+data.cardNum+'" src="'+data.audioSrc+'" preload="auto"></audio>\
                                         </div>\
                                     </div>';
     })
+}
+
+function togglePlayPause(cardNum) {
+    const audio = document.getElementById('audio' + cardNum);
+    audio.volume = 0.1; // Set the fixed volume to 0.1
+    const playPauseBtn = document.getElementById('play-pause-btn' + cardNum);
+
+    if (audio.paused) {
+        audio.play();
+        playPauseBtn.innerHTML = '<i class="fa-solid fa-volume-high"></i>';
+    } else {
+        audio.pause();
+        playPauseBtn.innerHTML = '<i class="fa-solid fa-volume-mute"></i>';
+    }
+}
+
+function resetAudio(cardNum) {
+    const audio = document.getElementById('audio' + cardNum);
+    const playPauseBtn = document.getElementById('play-pause-btn' + cardNum);
+
+    audio.pause();
+    audio.currentTime = 0;
+    playPauseBtn.innerHTML = '<i class="fa-solid fa-volume-mute"></i>';
 }
 
 function handleClick1() {
